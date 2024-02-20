b69f8507dec36e8069222b7d1331b7527a9c7047
jaller698 <c.soetorp@gmail.com>
2024-02-20 11:07:42 +0100

    Remove unused code + comments

--- Backend/test/test_rest_api.cpp
+++ Backend/test/test_rest_api.cpp
@@ -33,19 +33,26 @@ protected:
         http_response response = client.request(request).get();
         return response;
     }
+
+    http_response sendPutRequest(const std::string& endpoint, const web::json::value& jsonBody) {
+        web::http::client::http_client client("http://localhost:8080");
+        web::http::http_request request(web::http::methods::PUT);
+        request.set_request_uri(endpoint);
+        request.set_body(jsonBody);
+        request.headers().set_content_type(U("application/json"));
+        http_response response = client.request(request).get();
+        return response;
+    }
 };
 
 TEST_F(RestAPIEndpointTest, ValidGetRequest) {
-    RestAPIEndpoint endpoint;
 
     // Send a valid GET request
     auto response = sendGetRequest("/api/endpoint");
-    //std::cout << response.extract_json() << std::endl;
     ASSERT_EQ(status_codes::OK, response.status_code());
 }
 
 TEST_F(RestAPIEndpointTest, ValidPostRequest) {
-    RestAPIEndpoint endpoint;
 
     // Create a JSON object for the request body
     web::json::value requestBody;
@@ -54,21 +61,27 @@ TEST_F(RestAPIEndpointTest, ValidPostRequest) {
 
     // Send a valid POST request
     auto response = sendPostRequest("/api/endpoint", requestBody);
-    //std::cout << response.extract_json() << std::endl;
-    // TODO: Add your assertions here
-    // For example, you can check if the response status code is OK
     ASSERT_EQ(status_codes::OK, response.status_code());
 }
 
 TEST_F(RestAPIEndpointTest, InvalidPostRequest) {
-    RestAPIEndpoint endpoint;
 
     // Create an empty JSON object for the request body
     web::json::value requestBody;
 
     // Send an invalid POST request
     auto response = sendPostRequest("/api/endpoint", requestBody);
-        // TODO: Add your assertions here
-    // For example, you can check if the response status code is BadRequest
     ASSERT_EQ(status_codes::BadRequest, response.status_code());
+}
+
+TEST_F (RestAPIEndpointTest, ValidPutRequest) {
+
+    // Create a JSON object for the request body
+    web::json::value requestBody;
+    requestBody[U("name")] = web::json::value::string(U("John"));
+    requestBody[U("age")] = web::json::value::number(25);
+
+    // Send a valid PUT request, currently not implemented
+    auto response = sendPutRequest("/api/endpoint", requestBody);
+    ASSERT_EQ(status_codes::NotImplemented, response.status_code());
 }

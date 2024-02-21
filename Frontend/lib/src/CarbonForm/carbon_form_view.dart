393ea30e43bbab1fef7b75d28ba4a49c67bd1758
Perry02 <59411123+Perry02@users.noreply.github.com>
2024-02-21 14:22:07 +0100

    changed protocal naming and added a example json files

---
+++ Frontend/lib/src/CarbonForm/carbon_form_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CarbonFormView extends StatelessWidget {
+  const CarbonFormView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Form(
+      child: Text('test'),
+    );
+  }
+}

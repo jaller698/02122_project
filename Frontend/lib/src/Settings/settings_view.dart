46cd53ed38ae103c67b744052603a8147c63139c
Perry02 <59411123+Perry02@users.noreply.github.com>
2024-03-13 17:30:10 +0100

    Update settings_view.dart

--- Frontend/lib/src/Settings/settings_view.dart
+++ Frontend/lib/src/Settings/settings_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
 
-class SettingsView extends StatelessWidget {
+class SettingsView extends StatefulWidget {
   const SettingsView({
     super.key,
   });
@@ -8,7 +9,28 @@ class SettingsView extends StatelessWidget {
   static const routeName = '/Settings';
 
   @override
+  State<SettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<SettingsView> {
+  final _formKey = GlobalKey<FormBuilderState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings'),
+      ),
+      body: SingleChildScrollView(
+        child: FormBuilder(
+          key: _formKey,
+          child: Column(
+            children: <Widget>[
+              FormBuilderSwitch(name: 'darkmode', title: const Text('Darkmode'))
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }

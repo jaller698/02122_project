e264e53bfbc236c37fc29cd18ec2797ff50a3ced
Perry02 <59411123+Perry02@users.noreply.github.com>
2024-03-19 20:11:01 +0100

    Update carbon_tracker_view.dart

--- Frontend/lib/src/CarbonTracker/carbon_tracker_view.dart
+++ Frontend/lib/src/CarbonTracker/carbon_tracker_view.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 
 class CarbonTrackerView extends StatelessWidget {
-  const CarbonTrackerView({
-    super.key,
-  });
+  const CarbonTrackerView({super.key});
+
+  static const List<CarbonTrackItem> items = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView.separated(
         itemBuilder: (context, index) {
-          return Placeholder(
-            fallbackHeight: 100,
-            child: Center(
-              child: Card(
-                child: Text('task - $index'),
+          return ListTile(
+            leading: const Card(
+              child: Padding(
+                padding: EdgeInsets.all(6.0),
+                child: Icon(
+                  Icons.directions_car,
+                  size: 34,
+                ),
               ),
             ),
+            title: const Text('Car'),
+            subtitle: Text(
+                'At 15:38 - ${DateTime.now().day}/${DateTime.now().month}/${DateTime.now().year}'),
+            trailing: const Text('42g'),
           );
         },
         separatorBuilder: (context, index) {
@@ -28,9 +35,63 @@ class CarbonTrackerView extends StatelessWidget {
         label: const Text('Add task'),
         icon: const Icon(Icons.add),
         onPressed: () {
-          showLicensePage(context: context);
+          showModalBottomSheet<void>(
+            context: context,
+            builder: (context) {
+              return SizedBox(
+                height: 200,
+                child: ListView(
+                  children: const <Widget>[
+                    ListTile(
+                      leading: Icon(Icons.explore),
+                      title: Text('Transport'),
+                    ),
+                    ListTile(
+                      leading: Icon(Icons.restaurant),
+                      title: Text('Food'),
+                    )
+                  ],
+                ),
+              );
+            },
+          );
         },
       ),
     );
   }
 }
+
+class CarbonTrackItem {}
+
+// to track
+//  travel
+//    by foot
+//    car
+//      electric
+//        hybrid
+//      petrol
+//      diesel
+//    bus
+//    train
+//    plane
+//      distance (choose in settings?)
+//      time
+//  food
+//    meat
+//      fish
+//      meat (low to high)
+//    dairy
+//    vegan
+//  shopping
+//    clothes
+//      fast fashion
+//    electronics (large to small)
+//  energy consumption 
+//    region
+//      coal
+//      natrual gas
+//      wind
+//      solar
+//      hydro
+//      nuclear
+//    by month?

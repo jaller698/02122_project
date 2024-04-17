9f4c785674a0ece143559ae1cca7a94125ac2ed5
NA33AT0R <Gabrielllarsen@outlook.com>
2024-04-17 15:24:31 +0200

    can now read userScore in flutter

--- Frontend/lib/src/CarbonStats/Carbon_stat_controller.dart
+++ Frontend/lib/src/CarbonStats/Carbon_stat_controller.dart
@@ -5,13 +5,12 @@ import 'package:carbon_footprint/src/Settings/settings_controller.dart';
 import 'package:http/http.dart' as http;
 
 class CarbonStatController {
-  
-  double fromJson(Map<String, dynamic> json) {
-    return switch (json) {
+  double fromJsonScore(Map<String, dynamic> answer) {
+    return switch (answer) {
       {
         'Score': double score,
       } =>
-       score,
+        score,
       _ => throw const FormatException('Failed to decode Klefulnech'),
     };
   }
@@ -21,26 +20,30 @@ class CarbonStatController {
     return 4.1;
   }
 
-  Future<double> fetchStats(String username) async {
+  Future<String> fetchStats(String username) async {
     //We dont actually get anything here, because the JSON request doesnt contain the user. And i dont know how to add it.
 
-  http.Request request = http.Request("GET", Uri.parse('${SettingsController.address}/userScore'));
-  request.body = jsonEncode(<String, String>{
-    'User': username,
-  });
-  request.headers.addAll(<String, String>{
-    'Content-Type': 'application/json; charset=UTF-8',
-  });
+    http.Request request = http.Request(
+        "GET", Uri.parse('${SettingsController.address}/userScore'));
+    request.body = jsonEncode(<String, String>{
+      'User': username,
+    });
+    request.headers.addAll(<String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    });
 
-  var response = await request.send();
-  //RESPONSE BURDE INDEHOLDE DATA'EN under "Score", but i dont know how to extract it
+    var response = await request.send();
+    var thing= await response.stream.transform(utf8.decoder).first;
 
 
+    //RESPONSE BURDE INDEHOLDE DATA'EN under "Score", but i dont know how to extract it
+    
     if (response.statusCode == 200) {
       // If the server did return a 200 OK response,
       // then parse the JSON.""
       //response[0];
-      return 3;
+   
+       return thing;
       //fromJson(jsonDecode() as Map<String, dynamic>);
     } else {
       // If the server did not return a 200 OK response,
--- Frontend/lib/src/CarbonStats/Carbon_stat_view.dart
+++ Frontend/lib/src/CarbonStats/Carbon_stat_view.dart
@@ -1,45 +1,44 @@
 import 'package:flutter/material.dart';
 
-import 'package:primer_progress_bar/primer_progress_bar.dart';
-
 import 'Carbon_stat_controller.dart';
 import 'package:carbon_footprint/src/user_controller.dart';
+
 class CarbonStatView extends StatelessWidget {
   const CarbonStatView({super.key});
-  
 
-  static List<Segment> segments = [
-    const Segment(value: 29, color: Colors.purple, label: Text("Transport")),
-    const Segment(value: 20, color: Colors.deepOrange, label: Text("Home")),
-    const Segment(value: 18, color: Colors.lime, label: Text("Food")),
-    const Segment(value: 33, color: Colors.green, label: Text("Other")),
-  ];
-
-  @override
   static final CarbonStatController _carbonController = CarbonStatController();
 
   @override
-  Widget build(BuildContext context) { 
-    
-    String k = "hihe";
-  //  _carbonController.readStats().then(((value) {
-  //     k = value.toString();
-  //   }));
+  Widget build(BuildContext context) {
+    //  _carbonController.readStats().then(((value) {
+    //     k = value.toString();
+    //   }));
     //FIgure out how to go from Future<double> to String.
-   
-     k = _carbonController.fetchStats( UserController().username).toString();
-    return ListView(
-      children:  <Widget>[
-       Placeholder(
-        fallbackHeight: 400,
-        child: Center(
-          child: Card(
-            child: Text(k),
-          ),
-        ),
-
-
-      ),
-    ]);
+//(_carbonController.fetchStats(UserController().username)).toString()
+    return FutureBuilder(
+      future: _carbonController.fetchStats(UserController().username),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+              itemCount: 1,
+              itemBuilder: (context, index) {
+                String k = "hihe";
+                k = snapshot.data!;
+                return Placeholder(
+                  fallbackHeight: 400,
+                  child: Center(
+                    child: Card(
+                      child: Text(k),
+                    ),
+                  ),
+                );
+              });
+        } else if (snapshot.hasError) {
+          return Text('error: ${snapshot.error.toString()}');
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
+    );
   }
 }

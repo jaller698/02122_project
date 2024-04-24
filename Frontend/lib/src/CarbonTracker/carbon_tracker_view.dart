7faee09b6764c44967c9f08382df4ffc67d56cec
Perry02 <59411123+Perry02@users.noreply.github.com>
2024-04-24 15:21:24 +0200

    Finished carbon tracker input

--- Frontend/lib/src/CarbonTracker/carbon_tracker_view.dart
+++ Frontend/lib/src/CarbonTracker/carbon_tracker_view.dart
@@ -1,5 +1,7 @@
 import 'package:carbon_footprint/src/CarbonTracker/carbon_tracker_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 class CarbonTrackerView extends StatelessWidget {
   CarbonTrackerView({super.key});
@@ -27,7 +29,7 @@ class CarbonTrackerView extends StatelessWidget {
                       leading: Icon(item.type.icon),
                       title: Text(item.type.text),
                       subtitle: Text(
-                          '${item.dateAdded.hour}:${item.dateAdded.second}'),
+                          '${item.dateAdded.hour}:${item.dateAdded.second} - ${item.dateAdded.day}/${item.dateAdded.month}/${item.dateAdded.year}'),
                       trailing: Text(item.carbonScore.toString()),
                       onTap: () {
                         control.removeTrackerItem(item.id!);
@@ -88,74 +90,11 @@ class CarbonTrackerView extends StatelessWidget {
                       leading: Icon(category.icon),
                       title: Text(category.name),
                       onTap: () {
-                        // if single item
-                        if (category.types.length == 1) {
-                          control.addTrackerItem(
-                            CarbonTrackerItem(
-                              category.types[0].name,
-                              category.types[0],
-                              4000,
-                              DateTime.now(),
-                            ),
-                          );
-                          if (singleAction) {
-                            Navigator.pop(context);
-                          }
-                        } else if (category.types.isNotEmpty) {
-                          showModalBottomSheet(
-                            constraints: const BoxConstraints(maxHeight: 400),
-                            context: context,
-                            showDragHandle: true,
-                            isScrollControlled: true,
-                            builder: (context) {
-                              return Column(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  const Text(
-                                    'Long press for additional options',
-                                    style: TextStyle(
-                                        fontSize: 12,
-                                        fontStyle: FontStyle.italic),
-                                  ),
-                                  SizedBox(
-                                    height: 335,
-                                    child: ListView.builder(
-                                      padding: const EdgeInsets.all(8.0),
-                                      itemCount: category.types.length,
-                                      itemBuilder: (context, index) {
-                                        final type = category.types[index];
-                                        return Card(
-                                          child: ListTile(
-                                            style: ListTileStyle.list,
-                                            leading: Icon(type.icon),
-                                            title: Text(type.text),
-                                            onTap: () {
-                                              showModalBottomSheet(
-                                                context: context,
-                                                builder: (context) {
-                                                  return SizedBox(
-                                                    height: 200,
-                                                  );
-                                                },
-                                              );
-
-                                              //control.addTrackerItem(CarbonTrackerItem(type.name,type,4000,DateTime.now(),),);
-                                              if (singleAction) {
-                                                Navigator.pop(context);
-                                                Navigator.pop(context);
-                                              }
-                                            },
-                                            onLongPress: () {},
-                                          ),
-                                        );
-                                      },
-                                    ),
-                                  ),
-                                ],
-                              );
-                            },
-                          );
-                        }
+                        showCarbonItemList(context, singleAction, category);
+                      },
+                      onLongPress: () {
+                        singleAction = false;
+                        showCarbonItemList(context, singleAction, category);
                       },
                     ),
                   );
@@ -165,6 +104,146 @@ class CarbonTrackerView extends StatelessWidget {
           ],
         );
       },
+    );
+  }
+
+  void showCarbonItemList(
+      BuildContext context, bool singleAction, CarbonTrackerCategory category) {
+    // if single item
+    if (category.types.length == 1) {
+      control.addTrackerItem(
+        CarbonTrackerItem(
+          category.types[0].name,
+          category.types[0],
+          4000,
+          DateTime.now(),
+        ),
+      );
+      if (singleAction) {
+        Navigator.pop(context);
+      }
+    } else if (category.types.isNotEmpty) {
+      showModalBottomSheet(
+        constraints: const BoxConstraints(maxHeight: 400),
+        context: context,
+        showDragHandle: true,
+        isScrollControlled: true,
+        builder: (context) {
+          return SizedBox(
+            height: 500,
+            child: ListView.builder(
+              padding: const EdgeInsets.all(8.0),
+              itemCount: category.types.length,
+              itemBuilder: (context, index) {
+                final type = category.types[index];
+                return Card(
+                  child: ListTile(
+                    style: ListTileStyle.list,
+                    leading: Icon(type.icon),
+                    title: Text(type.text),
+                    onTap: () {
+                      switch (type.type) {
+                        case CarbonTrackInputTypes.time:
+                        case CarbonTrackInputTypes.distance:
+                          showModalBottomSheet(
+                            context: context,
+                            builder: (context) {
+                              return SizedBox(
+                                height: 400,
+                                width: MediaQuery.of(context).size.width,
+                                child: TrackerInputDistance(
+                                    type: type,
+                                    control: control,
+                                    singleAction: singleAction),
+                              );
+                            },
+                          );
+                        case CarbonTrackInputTypes.single:
+                        default:
+                          control.addTrackerItem(
+                            CarbonTrackerItem(
+                              type.name,
+                              type,
+                              1,
+                              DateTime.now(),
+                            ),
+                          );
+                          if (singleAction) {
+                            Navigator.pop(context);
+                            Navigator.pop(context);
+                          }
+                      }
+                    },
+                  ),
+                );
+              },
+            ),
+          );
+        },
+      );
+    }
+  }
+}
+
+class TrackerInputDistance extends StatefulWidget {
+  const TrackerInputDistance({
+    super.key,
+    required this.type,
+    required this.control,
+    required this.singleAction,
+  });
+
+  final CarbonTackerType type;
+  final CarbonTrackerController control;
+  final bool singleAction;
+
+  @override
+  State<TrackerInputDistance> createState() => _TrackerInputDistanceState();
+}
+
+class _TrackerInputDistanceState extends State<TrackerInputDistance> {
+  int _currentValue = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: NumberPicker(
+            minValue: 0,
+            maxValue: 100,
+            value: _currentValue,
+            onChanged: (value) {
+              setState(() => _currentValue = value);
+            },
+            haptics: true,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: FilledButton(
+            onPressed: () {
+              widget.control.addTrackerItem(
+                CarbonTrackerItem(
+                  widget.type.name,
+                  widget.type,
+                  _currentValue,
+                  DateTime.now(),
+                ),
+              );
+              if (widget.singleAction) {
+                Navigator.pop(context);
+                Navigator.pop(context);
+                Navigator.pop(context);
+              }
+            },
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: const Text('Confirm'),
+            ),
+          ),
+        )
+      ],
     );
   }
 }

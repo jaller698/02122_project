b2e2a16935afcb28c46e62f1f1fee54cd4adea0a
Perry02 <59411123+Perry02@users.noreply.github.com>
2024-05-01 12:52:23 +0200

    Update dashboard_view.dart

--- Frontend/lib/src/Dashboard/dashboard_view.dart
+++ Frontend/lib/src/Dashboard/dashboard_view.dart
@@ -11,12 +11,7 @@ class DashboardView extends StatefulWidget {
   const DashboardView({
     super.key,
   });
-  static const List<String> labels = [
-    "Transport",
-    "Home",
-    "Food",
-    "Other"
-  ];
+  static const List<String> labels = ["Transport", "Home", "Food", "Other"];
   static List<Segment> segments = [
     Segment(value: 34, color: Colors.lightGreenAccent, label: Text(labels[0])),
     Segment(value: 20, color: Colors.orangeAccent, label: Text(labels[1])),
@@ -27,43 +22,39 @@ class DashboardView extends StatefulWidget {
   //static ThemeMode tm = _settingsService.themeMode() as ThemeMode;
   @override
   State<DashboardView> createState() => _DashboardViewState();
-
 }
 
 class _DashboardViewState extends State<DashboardView> {
-
-   final SettingsController _settingsController = SettingsController();
+  final SettingsController _settingsController = SettingsController();
 
   @override
   Widget build(BuildContext context) {
     final progressBar = PrimerProgressBar(segments: DashboardView.segments);
-    final pieChart = PieChart(
-                PieChartData(
-                  pieTouchData: PieTouchData(
-                    touchCallback: (FlTouchEvent event, pieTouchResponse) {
-                      setState(() {
-                        if (!event.isInterestedForInteractions ||
-                            pieTouchResponse == null ||
-                            pieTouchResponse.touchedSection == null) {
-                          DashboardView.touchedIndex = -1;
-                          return;
-                        }
-                        DashboardView.touchedIndex = pieTouchResponse
-                            .touchedSection!.touchedSectionIndex;
-                      });
-                    },
-                  ),
-                  
-                  startDegreeOffset: 180,
-                  borderData: FlBorderData(
-                    show: false,
-                  ),
-                  
-                  sectionsSpace: 1,
-                  centerSpaceRadius: 40,
-                  sections: showingSections(),// Optional
-        )
-      );
+    final pieChart = PieChart(PieChartData(
+      pieTouchData: PieTouchData(
+        touchCallback: (FlTouchEvent event, pieTouchResponse) {
+          setState(() {
+            if (!event.isInterestedForInteractions ||
+                pieTouchResponse == null ||
+                pieTouchResponse.touchedSection == null) {
+              DashboardView.touchedIndex = -1;
+              return;
+            }
+            DashboardView.touchedIndex =
+                pieTouchResponse.touchedSection!.touchedSectionIndex;
+          });
+        },
+      ),
+
+      startDegreeOffset: 180,
+      borderData: FlBorderData(
+        show: false,
+      ),
+
+      sectionsSpace: 1,
+      centerSpaceRadius: 40,
+      sections: showingSections(), // Optional
+    ));
     return ListView(
       children: <Widget>[
         AspectRatio(
@@ -76,44 +67,59 @@ class _DashboardViewState extends State<DashboardView> {
               Expanded(
                 child: AspectRatio(
                   aspectRatio: 0.5,
-                  child: pieChart ,
+                  child: pieChart,
                 ),
               ),
-            
               Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    Indicator(
-                      color: DashboardView.segments[0].color,
-                      text: DashboardView.labels[0],
-                      isSquare: false,
-                      size: DashboardView.touchedIndex == 0 ? 40 : 40,
-                      textColor: DashboardView.touchedIndex == 0 ? (_settingsController.themeMode == ThemeMode.dark ? Colors.white : Colors.black) : Colors.grey,
-                    ),
-                     Indicator(
-                      color: DashboardView.segments[1].color,
-                      text: DashboardView.labels[1],
-                      isSquare: false,
-                      size: DashboardView.touchedIndex == 1 ? 40 : 40,
-                      textColor: DashboardView.touchedIndex == 1 ? (_settingsController.themeMode == ThemeMode.dark ? Colors.white : Colors.black) : Colors.grey,
-                    ),
-                    Indicator(
-                      color: DashboardView.segments[2].color,
-                      text: DashboardView.labels[2],
-                      isSquare: false,
-                      size: DashboardView.touchedIndex == 2 ? 40 : 40,
-                      textColor: DashboardView.touchedIndex == 2 ? (_settingsController.themeMode == ThemeMode.dark ? Colors.white : Colors.black) : Colors.grey,
-                    ),
-                   Indicator(
-                      color: DashboardView.segments[3].color,
-                      text: DashboardView.labels[3],
-                      isSquare: false,
-                      size: DashboardView.touchedIndex == 3 ? 40 : 40,
-                      textColor: DashboardView.touchedIndex == 3 ? (_settingsController.themeMode == ThemeMode.dark ? Colors.white : Colors.black) : Colors.grey,
-                    ),
-                  ],
-                ),
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Indicator(
+                    color: DashboardView.segments[0].color,
+                    text: DashboardView.labels[0],
+                    isSquare: false,
+                    size: DashboardView.touchedIndex == 0 ? 40 : 40,
+                    textColor: DashboardView.touchedIndex == 0
+                        ? (_settingsController.themeMode == ThemeMode.dark
+                            ? Colors.white
+                            : Colors.black)
+                        : Colors.grey,
+                  ),
+                  Indicator(
+                    color: DashboardView.segments[1].color,
+                    text: DashboardView.labels[1],
+                    isSquare: false,
+                    size: DashboardView.touchedIndex == 1 ? 40 : 40,
+                    textColor: DashboardView.touchedIndex == 1
+                        ? (_settingsController.themeMode == ThemeMode.dark
+                            ? Colors.white
+                            : Colors.black)
+                        : Colors.grey,
+                  ),
+                  Indicator(
+                    color: DashboardView.segments[2].color,
+                    text: DashboardView.labels[2],
+                    isSquare: false,
+                    size: DashboardView.touchedIndex == 2 ? 40 : 40,
+                    textColor: DashboardView.touchedIndex == 2
+                        ? (_settingsController.themeMode == ThemeMode.dark
+                            ? Colors.white
+                            : Colors.black)
+                        : Colors.grey,
+                  ),
+                  Indicator(
+                    color: DashboardView.segments[3].color,
+                    text: DashboardView.labels[3],
+                    isSquare: false,
+                    size: DashboardView.touchedIndex == 3 ? 40 : 40,
+                    textColor: DashboardView.touchedIndex == 3
+                        ? (_settingsController.themeMode == ThemeMode.dark
+                            ? Colors.white
+                            : Colors.black)
+                        : Colors.grey,
+                  ),
+                ],
+              ),
             ],
           ),
         ),
@@ -130,6 +136,7 @@ class _DashboardViewState extends State<DashboardView> {
               flex: 2,
               child: Placeholder(
                 fallbackHeight: 150,
+                child: Text('last 7 days of carbon usage'),
               ),
             ),
             Expanded(
@@ -156,12 +163,15 @@ class _DashboardViewState extends State<DashboardView> {
               flex: 1,
               child: Placeholder(
                 fallbackHeight: 150,
+                child: Text('tips og reminders til at forminske ens forbrug'),
               ),
             ),
             Expanded(
               flex: 2,
               child: Placeholder(
                 fallbackHeight: 150,
+                child:
+                    Text('Hvis alle gjorde som dig, hvordan vil jorden se ud?'),
               ),
             ),
           ],
@@ -182,40 +192,44 @@ class _DashboardViewState extends State<DashboardView> {
   List<PieChartSectionData> showingSections() {
     return List.generate(
       4,
-     (i) {
+      (i) {
         final isTouched = i == DashboardView.touchedIndex;
         Color color0 = DashboardView.segments[0].color;
         Color color1 = DashboardView.segments[1].color;
         Color color2 = DashboardView.segments[2].color;
         Color color3 = DashboardView.segments[3].color;
 
-     
         double? val0 = DashboardView.segments[0].value.toDouble();
         double? val1 = DashboardView.segments[1].value.toDouble();
         double? val2 = DashboardView.segments[2].value.toDouble();
         double? val3 = DashboardView.segments[3].value.toDouble();
-        double? total = val0+val1+val2+val3;
+        double? total = val0 + val1 + val2 + val3;
 
-        String? title0 = (val0/total * 100).toStringAsFixed(1) + "%";
-        String? title1 = (val1/total * 100).toStringAsFixed(1) + "%";
-        String? title2 = (val2/total * 100).toStringAsFixed(1) + "%";
-        String? title3 = (val3/total * 100).toStringAsFixed(1) + "%";
+        String? title0 = (val0 / total * 100).toStringAsFixed(1) + "%";
+        String? title1 = (val1 / total * 100).toStringAsFixed(1) + "%";
+        String? title2 = (val2 / total * 100).toStringAsFixed(1) + "%";
+        String? title3 = (val3 / total * 100).toStringAsFixed(1) + "%";
         switch (i) {
           case 0:
             return PieChartSectionData(
               color: color0,
               value: val0,
-              titleStyle: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold),
+              titleStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
               title: title0,
               radius: isTouched ? 110 : 80,
               titlePositionPercentageOffset: 0.55,
-              
             );
           case 1:
             return PieChartSectionData(
               color: color1,
               value: val1,
-              titleStyle: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold),
+              titleStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
               title: title1,
               radius: isTouched ? 110 : 80,
               titlePositionPercentageOffset: 0.55,
@@ -224,7 +238,10 @@ class _DashboardViewState extends State<DashboardView> {
             return PieChartSectionData(
               color: color2,
               value: val2,
-              titleStyle: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold),
+              titleStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
               title: title2,
               radius: isTouched ? 110 : 80,
               titlePositionPercentageOffset: 0.55,
@@ -233,7 +250,10 @@ class _DashboardViewState extends State<DashboardView> {
             return PieChartSectionData(
               color: color3,
               value: val3,
-              titleStyle: TextStyle(color: Colors.black, fontSize: 22, fontWeight: FontWeight.bold),
+              titleStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
               title: title3,
               radius: isTouched ? 110 : 80,
               titlePositionPercentageOffset: 0.55,
@@ -241,7 +261,7 @@ class _DashboardViewState extends State<DashboardView> {
           default:
             throw Error();
         }
-      }, 
+      },
     );
   }
 }

29903e30fc83668e41c9dc176c1d3076cdba0763
jaller698 <c.soetorp@gmail.com>
2024-06-12 13:16:07 +0200

    add random tip

--- Frontend/lib/src/Dashboard/dashboard_view.dart
+++ Frontend/lib/src/Dashboard/dashboard_view.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:carbon_footprint/src/Dashboard/carbon_score_pie_chart.dart';
 import 'package:carbon_footprint/src/Dashboard/carbon_score_widget.dart';
+import 'package:carbon_footprint/src/Dashboard/dashboard_controller.dart';
 import 'package:carbon_footprint/src/user_controller.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +20,20 @@ class DashboardView extends StatefulWidget {
 }
 
 class _DashboardViewState extends State<DashboardView> {
+
+  get_random_tips() {
+    // return a random tip
+    var random_facts = [
+      "Carbon dioxide consists of one carbon atom and two oxygen atoms, making its chemical formula CO2.",
+      "CO2 is what gives soda and sparkling water their fizz.",
+      "CO2 is used in fire extinguishers because it can suffocate flames by displacing oxygen",
+      "CO2 is often added to greenhouses to enhance plant growth.",
+      "CO2 is a major greenhouse gas, trapping heat in the atmosphere and contributing to global warming.",
+      "CO2 dissolves in seawater, forming carbonic acid, which lowers the pH of oceans and affects marine life."
+    ];
+    return random_facts[Random().nextInt(random_facts.length)];
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -30,12 +47,19 @@ class _DashboardViewState extends State<DashboardView> {
           child: WeekSummaryBarChart(),
         ),
         const Divider(),
-        const Row(children: [
+        Row(children: [
           Expanded(
             flex: 1,
-            child: Placeholder(
-              fallbackHeight: 150,
-              child: Text('tips og reminders til at forminske ens forbrug'),
+            child: Column(
+              children: [
+                Text(
+                  get_random_tips(),
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
           ),
         ]),
